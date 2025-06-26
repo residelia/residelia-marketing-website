1a427ed76bfe944319732eeea4f305a56912ec69
Arturo Díaz <arturo.diaz@residelia.com>
2025-06-26 09:07:30 +0200

    Intentando arreglar el tema de las urls

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -400,6 +400,7 @@ export default defineNuxtConfig({
 
   i18n: {
     // vueI18n: "@/i18n/config.ts", // if you are using custom path, default
+    baseUrl: process.env.BASE_URL,
     strategy: "prefix_except_default",
     locales: [
       { code: 'es', iso: 'es-ES', name: 'Español' },
