55a10eed37aeb790fcabd47d2584de3d577a2154
Arturo Díaz <arturo.diaz@residelia.com>
2026-02-12 16:41:25 +0100

    Upgrading logos on 2 rows

--- app/components/sliders/brandLogos.vue
+++ app/components/sliders/brandLogos.vue
@@ -2,20 +2,22 @@
     <div v-if="customers" class="brands-carousel-5">
         <ClientOnly>
             <swiper
-                :modules="[Autoplay, Mousewheel, Scrollbar, Thumbs]"
+                :modules="[Autoplay, Grid, Mousewheel, Scrollbar, Thumbs]"
                 :slides-per-view="2"
                 :loop="true"
+                :grid="{ rows: 2, fill: 'row' }"
+                :space-between="60"
                 :autoplay="{ delay: 3000, disableOnInteraction: false }"
                 :breakpoints="{
-                    400:{ slidesPerView:2 },
-                    600:{ slidesPerView:3 },
-                    900:{ slidesPerView:4 },
-                    1024:{ slidesPerView:5 }
+                    400:{ slidesPerView:2, grid:{ rows:2, fill:'row' } },
+                    600:{ slidesPerView:3, grid:{ rows:2, fill:'row' } },
+                    900:{ slidesPerView:4, grid:{ rows:2, fill:'row' } },
+                    1024:{ slidesPerView:5, grid:{ rows:2, fill:'row' } }
                 }"
             >
                 <swiper-slide v-for="slide in customers" :key="slide._id">
                     <div class="brand-logo">
-                        <img class="img-fluid theme-light align-self-center" :src="slide.logoBlack.url+'?fm=webp'" :alt="slide.alt" style="max-height: 50px; min-height: 20px; object-fit: contain; -o-object-fit: contain;"/>
+                        <img class="img-fluid theme-light align-self-center" :src="slide.logoBlack.url+'?fm=webp'" :alt="slide.alt" style="max-height: 50px; min-height: 15px; object-fit: contain; -o-object-fit: contain;"/>
                     </div>
                 </swiper-slide>
             </swiper>
@@ -26,12 +28,14 @@
 <script setup lang="ts">
 
 import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Autoplay, Mousewheel, Scrollbar, Thumbs } from 'swiper/modules';
+import { Autoplay, Grid, Mousewheel, Scrollbar, Thumbs } from 'swiper/modules';
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/grid';
 import { ClientOnly } from '#components';
 
 const props = defineProps<{
     customers: Array<any>
 }>();
+
 </script>
